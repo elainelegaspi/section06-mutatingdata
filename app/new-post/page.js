f8956256f26cb9a3912205736b12603f53b9ea36
elainelegaspi <143445067+elainelegaspi@users.noreply.github.com>
2025-06-12 11:03:39 -0500

    Validating user input with useFormState Hook

--- app/new-post/page.js
+++ app/new-post/page.js
@@ -1,5 +1,9 @@
+"use client";
+
+import { useFormSate } from 'react-dom';
 import { storePost } from '@/lib/posts';
 import { redirect } from 'next/navigation';
+import FormSubmit from '@/components/form-submit';
 
 export default function NewPostPage() {
   async function createPost(formData) {
@@ -7,6 +11,24 @@ export default function NewPostPage() {
     const title = formData.get('title');
     const image = formData.get('image');
     const content = formData.get('content');
+
+    let errors = [];
+
+    if (!title || title.trim().length ===0 ) {
+      errors.push("Title is required.");
+    }
+
+    if (!content || content.trim().length === 0) {
+       errors.push("Content is required.");
+    }
+
+    if (!image) {
+       errors.push("Image is required.");
+    }
+
+    if (errors.length > 0) {
+      return { errors };
+    }
 
     await storePost({
       imageUrl: '',
@@ -18,13 +40,15 @@ export default function NewPostPage() {
     redirect('/feed');
   }
 
+  const [state, formAction] = useFormState(createPost, {});
+
   return (
     <>
       <h1>Create a new post</h1>
-      <form action={createPost}>
+      <form action={formAction}>
         <p className="form-control">
           <label htmlFor="title">Title</label>
-          <input type="text" id="title" name="title" />
+          <input type="text" id="title" name="title"/>
         </p>
         <p className="form-control">
           <label htmlFor="image">Image URL</label>
